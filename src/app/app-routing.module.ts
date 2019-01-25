3e259d97d6088ea45afa171a2347f24a17803762
duncan <arnevit@msn.com>
2019-01-25 16:01:14 +0000

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './Header/app.Header';
+import { HomeComponent } from './ContentArea/app.Home';
+
+import { PageNotFoundComponent } from './PageNotFound/app.PageNotFound';
+
+
+
+const routes: Routes = [{path: '', component:  HomeComponent },
+
+
+                        {path: '**', redirectTo: 'not-found'}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true, scrollPositionRestoration: 'enabled' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule, Title  } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header/app.Header';
+import { FooterComponent } from './Footer/app.Footer';
+import { NavComponent } from './Nav/app.Nav';
+import { PageNotFoundComponent } from './PageNotFound/app.PageNotFound';
+
+import { HomeComponent } from './ContentArea/app.Home';
+
+
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    PageNotFoundComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [Title],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
