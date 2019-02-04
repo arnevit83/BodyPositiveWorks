446c4d00e337452a3a2a0913e032dc9841e1e32a
duncan <arnevit@msn.com>
2019-02-04 23:46:15 +0000

    Homepage Silder & IG Feed

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,31 +1,6 @@
 /*! Custom * !*/
-var images = [
-  "/assets/images/bg2.webp",
-  "/assets/images/bg3.webp",
 
-  "/assets/images/bg5.webp",
-  "/assets/images/bg1.webp"
-]
 $(document).ready(function() {
-  // Ugly Background change
-  //Still need to do the png backward compatility
-
-  var i = 0;
-  var backgroundchange = true
-  setInterval(function() {
-
-    if (backgroundchange) {
-      $(".page-header").css('background-image',"url(" + images[i] + ")").bind('onreadystatechange load', function(){
-        if (this.complete) $(this).fadeIn(300);
-        });
-      i = i + 1;
-      if (i == images.length) {
-        i = 0;
-      }    }
-    }, 8000);
-
-
-
 
   $('.ScrollUp').click(function() {
     $('html, body').animate({
@@ -36,19 +11,13 @@ $(document).ready(function() {
   });
 
   $(window).on("scroll", function() {
-    if ($(window).scrollTop()  > 800) {
+    if ($(window).scrollTop() > 800) {
       $('.ScrollUp').fadeIn();
-            backgroundchange = false;
+
     } else {
       $('.ScrollUp').fadeOut();
-      backgroundchange = true;
-    }
-    if ($(window).scrollTop()  > 100 ) {
-            backgroundchange = false;
-    } else {
-      backgroundchange = true;
-    }
 
+    }
   });
 
   $('.ScrollUp').hover(
@@ -59,14 +28,25 @@ $(document).ready(function() {
       $(this).removeClass('shadow')
     }
   )
-})
+  $('.gallery-feed').spectragram('getUserFeed', {
+    complete: galleryfeedkFunc(),
+    max: 8,
+    size: "small",
+    wrapEachWith: "<div class='IGFeed'></div>"
+  });
 
+});
 
+function galleryfeedkFunc() {
 
+  window.setTimeout(function() {
 
-$.fn.preload = function() {
-    this.each(function(){
-        $('<img/>')[0].src = this;
+    $(".gallery-feed").find("a").replaceWith(function() {
+      return $(this).contents();
     });
-}
-$(images).preload();
+  }, 3000);
+};
+window.setTimeout(function() {
+  $("#SliderBanner").children().children().css("display", "block");
+
+}, 2000);
