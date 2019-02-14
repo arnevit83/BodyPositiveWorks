3a32ca447ea3cc204c2df3e1211063f1f9b6e761
duncan <arnevit@msn.com>
2019-02-14 00:56:59 +0000

    Art Theropy Step2

--- src/app/ContentArea/Services/art-therapy/art-therapy.component.ts
+++ src/app/ContentArea/Services/art-therapy/art-therapy.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons';
+
+declare var $: any;
 
 @Component({
   selector: 'app-art-therapy',
@@ -6,10 +13,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./art-therapy.component.scss']
 })
 export class ArtTherapyComponent implements OnInit {
-
+  faTwitter = faTwitter;
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
+  faGooglePlusG = faGooglePlusG;
+  faCalendarAlt = faCalendarAlt;
   constructor() { }
 
   ngOnInit() {
+
+
   }
 
 }
