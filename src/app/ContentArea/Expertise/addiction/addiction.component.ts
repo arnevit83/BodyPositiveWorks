846a63677b8c6ca9e78bfaf3643a0da2ce212b6e
duncan <arnevit@msn.com>
2019-02-20 10:12:48 +0000

    addiction

--- src/app/ContentArea/Expertise/addiction/addiction.component.ts
+++ src/app/ContentArea/Expertise/addiction/addiction.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Meta } from '@angular/platform-browser';
+
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-addiction',
@@ -6,10 +15,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addiction.component.scss']
 })
 export class AddictionComponent implements OnInit {
-
-  constructor() { }
+  faTwitter = faTwitter;
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
+  faGooglePlusG = faGooglePlusG;
+  faCalendarAlt = faCalendarAlt;
+  faMapMarkerAlt = faMapMarkerAlt;
+  public constructor(private titleService: Title, private meta: Meta) {
+    this.titleService.setTitle("Addiction Therapists NJ");
+    this.meta.updateTag({ name: 'description', content: 'Body Positive Works are addiction specialists serving NJ, Bergen County and Rockland County. Our approach to addiction is holistic. Body Positive Works is a one-stop shop offering integrated addiction help (in particular drug addiction help) in the form of services, programs, and classes to support individuals on their journey to recovery.' });
+    //   this.meta.updateTag({ name: 'keywords', content: 'Freelance copywriter,Copywriter,Brand strategist,SEO copywriting,Brand strategies,Book Author,Author,Yoga books,Yoga memoir,Yoga School Dropout,Yoga shop' });
+  }
 
   ngOnInit() {
+
   }
 
 }
