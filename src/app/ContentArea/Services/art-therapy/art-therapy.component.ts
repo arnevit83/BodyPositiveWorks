ae516b2997d0d7746181c781e14d1742bd5d195d
duncan <arnevit@msn.com>
2019-03-14 10:19:42 +0000

    Art Therapy

--- src/app/ContentArea/Services/art-therapy/art-therapy.component.ts
+++ src/app/ContentArea/Services/art-therapy/art-therapy.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Meta } from '@angular/platform-browser';
+
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 declare var $: any;
 
@@ -18,7 +22,12 @@ export class ArtTherapyComponent implements OnInit {
   faInstagram = faInstagram;
   faGooglePlusG = faGooglePlusG;
   faCalendarAlt = faCalendarAlt;
-  constructor() { }
+  faMapMarkerAlt = faMapMarkerAlt;
+  public constructor(private titleService: Title, private meta: Meta) {
+    this.titleService.setTitle("Yoga Bergen County NJ – Winner Bergen Magazine Readers’ Choice Awards");
+    this.meta.updateTag({ name: 'description', content: 'Body Positive Works is proud to host one of the top nutritionists in Bergen County NJ. Specializing in emotional eating, overeating, binging, & food restricting. Individualized nutrition counseling. Addressing the mind & heart issues behind our relationship with food.  Safe & joyful community.' });
+    this.meta.updateTag({ name: 'keywords', content: 'Nutritionist, Nutritionist near you, Nutritionist nj, Nutritionist bergen county nj, Best nutritionist in nj, Top nutritionists in nj, Best nutritionist in bergen county, Nutrition services, Emotional eating, Overeating, Binge eating disorders' });
+  }
 
   ngOnInit() {
 
