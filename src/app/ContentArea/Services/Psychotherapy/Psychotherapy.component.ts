60298b6a9a81b326ba0106ae508421ec426ac6d0
duncan <arnevit@msn.com>
2019-03-14 16:09:33 +0000

    Psychotherapy

--- src/app/ContentArea/Services/Psychotherapy/Psychotherapy.component.ts
+++ src/app/ContentArea/Services/Psychotherapy/Psychotherapy.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Meta } from '@angular/platform-browser';
+
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-Psychotherapy',
@@ -7,7 +16,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PsychotherapyComponent implements OnInit {
 
-  constructor() { }
+  faTwitter = faTwitter;
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
+  faGooglePlusG = faGooglePlusG;
+  faCalendarAlt = faCalendarAlt;
+  faMapMarkerAlt = faMapMarkerAlt;
+  public constructor(private titleService: Title, private meta: Meta) {
+    this.titleService.setTitle("Holistic Psychotherapists Bergen NJ – Individuals, Groups and Families");
+    this.meta.updateTag({ name: 'description', content: 'Our psychotherapists have the highest licensure for Psychotherapy in NJ. Holistic approach with a variety of therapy counseling services under one roof. Specializing in anxiety, depression, negative body image, eating disorders, & substance addiction. Individual counseling, family and groups. Safe & joyful community.' });
+    this.meta.updateTag({ name: 'keywords', content: 'Psychotherapy, Psychotherapist, Psychotherapy in NJ, Psychotherapy in Bergen County NJ, Therapy near you, Therapy offices, Therapy centers, Therapy places, Therapy, Therapist' });
+  }
 
   ngOnInit() {
   }
