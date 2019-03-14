199ed48e68f530ec794af4f2ca5406015d9f9932
duncan <arnevit@msn.com>
2019-03-14 14:29:56 +0000

    Chiropractor

--- src/app/ContentArea/Services/chiropractic/chiropractic.component.ts
+++ src/app/ContentArea/Services/chiropractic/chiropractic.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Meta } from '@angular/platform-browser';
+
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-chiropractic',
@@ -7,8 +16,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChiropracticComponent implements OnInit {
 
-  constructor() { }
-
+  faTwitter = faTwitter;
+  faFacebook = faFacebook;
+  faInstagram = faInstagram;
+  faGooglePlusG = faGooglePlusG;
+  faCalendarAlt = faCalendarAlt;
+  faMapMarkerAlt = faMapMarkerAlt;
+  public constructor(private titleService: Title, private meta: Meta) {
+    this.titleService.setTitle("Licensed Chiropractor Serving Bergen County & Rockland County");
+    this.meta.updateTag({ name: 'description', content: 'Natural, drug free pain relief for adults, teens and children from one of the best holistic chiropractors in Bergen NJ; Dr. Victoria Dubin Master. Treating sciatica, neck pain, scoliosis, lower back pain, headaches, frozen shoulder and much more the gentle way – no pushing, pulling, or popping.' });
+    this.meta.updateTag({ name: 'keywords', content: 'Best chiropractor NJ, Best chiropractors in NJ, Chiropractor NJ, Chiropractor Montclair NJ, Physical therapy Bergen County NJ, Physical therapy Bergen County, Chiropractor Bergen County, Chiropractor Bergen County NJ, Physical therapy Closter NJ, Bergen physical therapy, Montvale chiropractor' });
+  }
   ngOnInit() {
   }
 
