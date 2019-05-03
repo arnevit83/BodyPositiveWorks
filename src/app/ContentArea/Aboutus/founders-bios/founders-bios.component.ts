73c61b93008e2aaf93e79cc3d3d9704d9015d9d7
duncan <arnevit@msn.com>
2019-05-03 10:31:39 +0100

    Image Changes

--- src/app/ContentArea/Aboutus/founders-bios/founders-bios.component.ts
+++ src/app/ContentArea/Aboutus/founders-bios/founders-bios.component.ts
@@ -10,7 +10,7 @@ export class FoundersBiosComponent implements OnInit {
 
   constructor(private titleService: Title, private meta: Meta) {  
   this.titleService.setTitle("Meet BoPo’s Co-Founders – Jen Kraft & Melanie Struble");
-  this.meta.updateTag({ name: 'description', content: 'Meet yoga teacher Jen Kraft and psychotherapist Melanie Struble – co-founders of BoPo Works and certified experts in the treatment of adolescents, adults and families dealing with depression, anxiety, eating disorders, negative body image, alcohol and drug addiction, and stage of life issues.' }); 
+  this.meta.updateTag({ name: 'description', content: 'Meet yoga teacher Jen Kraft and psychotherapist Melanie Struble – co-founders of BoPoWorks and certified experts in the treatment of adolescents, adults and families dealing with depression, anxiety, eating disorders, negative body image, alcohol and drug addiction, and stage of life issues.' }); 
   this.meta.updateTag({ name: 'keywords', content: 'Psychotherapist, Yoga Teacher, Eating Disorder, Depression, Anxiety, Negative body image, alcohol and drug addiction' });
 }
 
