e03e98ca06571c2bbefb0f2da1c9ca9df2412b06
duncan <arnevit@msn.com>
2019-05-19 10:54:02 +0100

    Step1

--- src/app/ContentArea/Events/Events.component.ts
+++ src/app/ContentArea/Events/Events.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Meta } from '@angular/platform-browser';
-
+import { HttpClient } from '@angular/common/http';
+declare var require: any
 
 
 @Component({
@@ -10,13 +11,22 @@ import { Meta } from '@angular/platform-browser';
   styleUrls: ['./Events.component.scss']
 })
 export class EventsComponent {
-
-  public constructor(private titleService: Title, private meta: Meta) {
+  events: any;
+  services: any;
+  
+  public constructor(private titleService: Title, private meta: Meta, private http: HttpClient ) {
     this.titleService.setTitle( "Events - Body Positive Works, Saddle River, Bergen County" );
     this.meta.updateTag({ name: 'description', content: 'Holistic one stop health & wellness center. Feel good inside & out with our integrated services, programs & classes. Our licensed therapists, the best in NJ, specialize in anxiety, depression, negative body image, eating disorders, & substance addiction. Nutritionists, Psychotherapists, Massage, Yoga, Pilates & more.' });
     this.meta.updateTag({ name: 'keywords', content: 'Yoga NJ, Yoga Bergen County, Yoga Saddle River NJ, Chiropractor NJ, Chiropractor Bergen County, Nutritionist nj, Nutritionist bergen county nj, Psychotherapy NJ, Psychotherapy in Bergen County NJ, Massage places NJ, Massage Westfield NJ, Massage Fair Lawn NJ, Pilates NJ, Pilates classes Bergen County NJ' });
+  }
+  ngOnInit() {
+    this.http.get("https://cms.justpeachysolutions.co.uk/categories?events.id_gte=1&_sort=CategoryTitle:ASC")
+    .subscribe((services) => this.services = services );
 
- 
+    const moment = require('moment');     
+    this.http.get("https://cms.justpeachysolutions.co.uk/events?StartDateTime_gte=" + moment().format('YYYY-MM-DD') + "&_sort=StartDateTime:ASC")
+    .subscribe((events) => this.events = events);
+
   }
 
 }
