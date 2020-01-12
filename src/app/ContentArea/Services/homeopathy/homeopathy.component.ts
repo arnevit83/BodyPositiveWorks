2da0b3f06043326b30daa57f4368eeafc96ac7a6
duncan <arnevit@msn.com>
2020-01-12 11:36:37 +0000

    Revert "Logs and Homepathy text"
    This reverts commit e2633bf658874c7c99f70d4b11e4f3a02c014e2c.

--- src/app/ContentArea/Services/homeopathy/homeopathy.component.ts
+++ src/app/ContentArea/Services/homeopathy/homeopathy.component.ts
@@ -24,7 +24,7 @@ export class HomeopathyComponent implements OnInit {
   faMapMarkerAlt = faMapMarkerAlt;
   public constructor(private titleService: Title, private meta: Meta) {
     this.titleService.setTitle("Homeopathy New Jersey – Personalized Treatments. Welcoming Community.");
-    this.meta.updateTag({ name: 'description', content: 'One of the best homeopaths in NJ. Holistic approach based on the needs of the individual. Homeopathic remedies prepared according to FDA recognized Homeopathic Pharmacopoeia. Specializing in homeopathy for fertility, allergies, colds and flu, depression and anxiety, headaches and weight loss. Individual consultations. Personalized treatments.' });
+    this.meta.updateTag({ name: 'description', content: 'One of the best homeopaths in NJ. Holistic approach based on the needs of the individual. Homeopathic remedies prepared according to FDA recognized Homeopathic Pharmacopoeia. Specializing in homeopathy for fertility, allergies, colds and flu, depression and anxiety, headaches and weight loss. Individual consultations. Personalized treatments. ' });
     this.meta.updateTag({ name: 'keywords', content: 'Homeopathy,What is Homeopathy?,Homeopathy treatment,How homeopathy works,Homeopathy New Jersey,Best homeopath in NJ,Homeopathy for fertility,Homeopathy for allergies,Homeopathy for sore throat,Homeopathy for cold,Homeopathy for depression,Homeopathy for anxiety,Homeopathy for flu,Homeopathy for headache,Homeopathy for weight loss,Homeopathy for eczema,Homeopathy for sinus infection,Homeopathy for UTI,ADHD Homeopathy' });
   }
 
